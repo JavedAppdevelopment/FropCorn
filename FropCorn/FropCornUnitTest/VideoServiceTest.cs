5db5ec9b98ef8dcf35fba2eba83d2d8df011c27e
JavedAppdevelopment <javedappdevelopment@gmail.com>
2016-06-22 00:26:21 +0530

    Added UnitTest Project.
    Added UnitTest Project.

---
+++ FropCorn/FropCornUnitTest/VideoServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FropCorn.Services;
+using System.Collections.Generic;
+using FropCorn;
+using System.Threading.Tasks;
+
+namespace FropCornUnitTest
+{
+    [TestClass]
+    public class VideoServiceTest
+    {
+        [TestMethod]
+        [Timeout(10000)]//Maximum Timeout 10 Second
+        public async Task TestMethod1()
+        {
+            VideoService videoService = new VideoService();
+            var response = await videoService.GetAllVideosBySearchAsync("sanam", "fropcorn");
+            Assert.IsInstanceOfType(response, typeof(List<VideosViewModel>));
+        }
+    }
+}
